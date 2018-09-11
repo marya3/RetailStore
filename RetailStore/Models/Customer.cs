65573e96ece2ad86cd07b138f361b0300540aa1b
Manoj Arya <manoj.arya@xlent.se>
2018-09-11 11:00:40 +0200

    Initial commit

---
+++ RetailStore/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailStore.Models
+{
+    public class Customer
+    {
+        public int customerId { get; set; }
+        public String firstName { get; set; }
+
+        public String lastName { get; set; }
+        public String street { get; set; }
+        public int zipCode { get; set; }
+        public String city { get; set; }
+        public String email { get; set; }
+    }
+}
